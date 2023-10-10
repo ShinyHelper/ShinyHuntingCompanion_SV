d4f20a3cd20f6a3555c98f695b3846996abd2a18
JackDixon019 <13709@coderacademy.edu.au>
2023-10-10 19:54:28 +1100

    feat: started adding search features - routing and params so far

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-        <Header></Header>
+        <Header />
         <Routes>
             <Route path='/' element={<App />} />
             <Route path='/counter' element={<OutbreakCounterPage />} />
