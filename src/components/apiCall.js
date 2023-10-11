0f1b6ce5a7e0d75661e25d94101d3bbfdf31960b
JackDixon019 <13709@coderacademy.edu.au>
2023-10-11 16:54:52 +1100

    Feat: api fetch request completed. (very) rough results page made

--- src/components/apiCall.js
+++ src/components/apiCall.js
@@ -1,18 +1,53 @@
 export default async function fetchData(url) {
     let response = await fetch(url);
-    console.log(response);
     let responseData = await response.json();
-    makeDataNice(responseData);
-    return responseData;
+    console.log(responseData)
+    let niceData = await makeDataNice(responseData);
+    console.log(niceData)
+    return niceData;
 }
 
 
+// this function strips unneeded data and formatting.
 function makeDataNice(data) {
-    console.log(data);
-    // example data layout:
-    // let niceData = {}
-    // niceData.types = []
-    // data.types.forEach(element => {
-    //     niceData.types.append(element.type.name)
-    // });
+    let niceData = {};
+    niceData.name = data.name;
+    niceData.types = [];
+    niceData.warning = [];
+    niceData.sprites = {
+        front_default: data.sprites.other["official-artwork"].front_default,
+        front_shiny: data.sprites.other["official-artwork"].front_shiny,
+    };
+
+    data.types.forEach((element) => {
+        niceData.types.push(element.name);
+    });
+
+    // These moves can all kill the pokemon that uses them. 
+    // Need to warn the user to bring countermeasures
+    let warningMoves = [
+        "brave-bird",
+        "chloroblast",
+        "double-edge",
+        "flare-blitz",
+        "head-charge",
+        "head-smash",
+        "self-destruct",
+        "shadow-end",
+        "shadow-rush",
+        "submission",
+        "take-down",
+        "volt-tackle",
+        "wave-crash",
+        "wild-charge",
+        "wood-hammer",
+    ];
+
+    data.moves.forEach((element) => {
+        if (warningMoves.includes(element.move.name)) {
+            niceData.warning.push(element.move.name);
+        }
+    });
+    console.log(niceData)
+    return niceData
 }
--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -8,20 +8,36 @@ export default function PokemonPage() {
     let { pokemonName } = useParams();
     let { activePokemon, setActivePokemon } = useContext(ActivePokemon);
     let { apiUrl } = useContext(ApiContext);
-    useEffect(() => {
-        async function getPokemonData(){
-            let urlToCall = apiUrl + pokemonName
-            await setActivePokemon(fetchData(urlToCall));
-        }
-        getPokemonData()
-    }, []);
 
-    return (
-        <div>
-            <h1>This is the page for {pokemonName}</h1>;
-            {<p>{activePokemon.name}</p> || <p>Searching</p>}
-        </div>
-    );
+    useEffect(() => {
+        setActivePokemon("");
+        getPokemonData();
+
+        async function getPokemonData() {
+            let pokemonData = await fetchData(apiUrl + pokemonName);
+            await setActivePokemon(pokemonData);
+        }
+    }, [pokemonName]);
+
+    if (!activePokemon.name) {
+        return <p>Searching</p>;
+    } else {
+        return (
+            <div>
+                <h1>This is the page for {pokemonName}</h1>
+                <div>
+                    <p>{activePokemon.name}</p>
+                    <p>Warning! This pokemon may have the following moves:</p>
+                    {activePokemon.warning.map((element) => {
+                        return <p>{element}</p>;
+                    })}
+                    <p>
+                        front sprite: <img src={activePokemon.sprites.front_default}></img>
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 async function callApi(pokemonName) {}
