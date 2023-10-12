8747c22a1765b1997d34769fe650c3f320438693
JackDixon019 <126362891+JackDixon019@users.noreply.github.com>
2023-10-12 11:05:50 +1100

    Feat: basic searchbar functionality added (#11)

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,10 +1,30 @@
-import { useParams } from "react-router-dom"
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export default function SearchBar(){
+export default function SearchBar() {
+    let [searchTerm, setSearchTerm] = useState();
+    const navigate = useNavigate();
 
-    return(
-        <div id="SearchBar">
-            This is the searchbar
-        </div>
-    )
+    function handleChange(event) {
+        setSearchTerm(event.target.value);
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        navigate("/search/" + searchTerm);
+    }
+
+    return (
+        <form action="search">
+            <input
+                type="text"
+                name="pokemon"
+                id="pokemon"
+                onChange={(event) => handleChange(event)}
+            />
+            <button type="submit" onClick={(event) => handleSubmit(event)}>
+                search
+            </button>
+        </form>
+    );
 }
