eec58b0b15dea6c8d49110501e9057d856075f5c
JackDixon019 <13709@coderacademy.edu.au>
2023-10-12 15:05:40 +1100

    Cut: removed unneeded files

--- src/contexts/allPokemon.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useState } from "react";
-
-
-let pokemonList = fetchAllPokemon()
-
-async function fetchAllPokemon(){
-    return await fetch("https://pokeapi.co/api/v2/pokemon/?limit=100000000");
-}
-
