8bb33a43ad177b5af216e6a099fd2b257ab181b7
JackDixon019 <13709@coderacademy.edu.au>
2023-10-13 11:34:55 +1100

    Feat: added sandwiches dictionary, added rudimentary HM toggle button

---
+++ src/components/sandwichesDictionary.js
@@ -0,0 +1,74 @@
+export default sandwichesDictionary = {
+    flying: {
+        nonHM: {ingredients: {"Egg": 4, "Prosciutto": 2}, seasonings: {"Ketchup": 1}}, 
+        HM: {ingredients: {"Prosciutto": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    rock: {
+        nonHM: {ingredients: {"Egg": 4, "Bacon": 2}, seasonings: {"Whipped Cream": 1}}, 
+        HM: {ingredients: {"Bacon": 1}, seasonings: {"Salty Herba Mystica": 1, "Sweet Herba Mystica": 1}}
+    },
+    steel: {
+        nonHM: {ingredients: {"Egg": 4, "Hamburger": 2}, seasonings: {"Ketchup": 1}}, 
+        HM: {ingredients: {"Hamburger": 1}, seasonings: {"Salty Herba Mystica": 1, "Sweet Herba Mystica": 1}}
+    },
+    grass: {
+        nonHM: {ingredients: {"Egg": 4, "Lettuce": 2}, seasonings: {"Ketchup": 1}}, 
+        HM: {ingredients: {"Lettuce": 1}, seasonings: {"Salty Herba Mystica": 1, "Spicy Herba Mystica": 1}}
+    },
+    ice: {
+        nonHM: {ingredients: {"Egg": 4, "Klawf Stick": 2}, seasonings: {"Ketchup": 1}}, 
+        HM: {ingredients: {"Klawf Stick": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    fairy: {
+        nonHM: {ingredients: {"Egg": 4, "Tomato": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Fairy": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    normal: {
+        nonHM: {ingredients: {"Chorizo": 4, "Tofu": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Tofu": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    fighting: {
+        nonHM: {ingredients: {"Pickle": 1, "Herbed Sausage": 2, "Rice": 3}, seasonings: {"Butter": 1}}, 
+        HM: {ingredients: {"Pickle": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    water: {
+        nonHM: {ingredients: {"Rice": 4, "Herbed Sausage": 2}, seasonings: {"Chilli Sauce": 1}}, 
+        HM: {ingredients: {"Cucumber": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    ground: {
+        nonHM: {ingredients: {"Noodles": 4, "Herbed Sausage": 2}, seasonings: {"Whipped Cream": 1}}, 
+        HM: {ingredients: {"Ham": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    psychic: {
+        nonHM: {ingredients: {"Noodles": 6}, seasonings: {"Vinegar": 1}}, 
+        HM: {ingredients: {"Onion": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    dark: {
+        nonHM: {ingredients: {"Potato Salad": 4, "Herbed Sausage": 2}, seasonings: {"Jam": 1}}, 
+        HM: {ingredients: {"Smoked Fillet": 1}, seasonings: {"Salty Herba Mystica": 1, "Sweet Herba Mystica": 1}}
+    },
+    ghost: {
+        nonHM: {ingredients: {"Potato Salad": 4, "Herbed Sausage": 2}, seasonings: {"Vinegar": 1}}, 
+        HM: {ingredients: {"Red Onion": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    poison: {
+        nonHM: {ingredients: {"Chorizo": 4, "Green Bell Pepper": 2}, seasonings: {"Horseradish": 1}}, 
+        HM: {ingredients: {"Noodles": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    electric: {
+        nonHM: {ingredients: {"Chorizo": 4, "Yellow Bell Pepper": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Yellow Bell Pepper": 1}, seasonings: {"Salty Herba Mystica": 1, "Spicy Herba Mystica": 1}}
+    },
+    bug: {
+        nonHM: {ingredients: {"Chorizo": 4, "Cherry Tomatoes": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Cherry Tomatoes": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+    fire: {
+        nonHM: {ingredients: {"Chorizo": 4, "Red Bell Pepper": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Red Bell Pepper": 1}, seasonings: {"Salty Herba Mystica": 1, "Spicy Herba Mystica": 1}}
+    },
+    dragon: {
+        nonHM: {ingredients: {"Chorizo": 4, "Avocado": 2}, seasonings: {"Salt": 1}}, 
+        HM: {ingredients: {"Avocado": 1}, seasonings: {"Salty Herba Mystica": 2}}
+    },
+}
--- src/pages/Sandwiches.jsx
+++ src/pages/Sandwiches.jsx
@@ -1,5 +1,21 @@
+import { useState } from "react"
+
 export default function SandwichesPage(){
+    let [herbaMystica, setHerbaMystica] = useState(false)
+    function handleClickHM(){
+        setHerbaMystica(true)
+    }
+
+    function handleClickNonHM(){
+        setHerbaMystica(false)
+    }
+
     return(
+        <div>
         <h1>SandwichesPage</h1>
+        <button id="herbaMysticaButton" onClick={handleClickHM}>Herba Mystica</button>
+        <button id="nonHerbaMysticaButton" onClick={handleClickNonHM}>Non-Herba Mystica</button>
+        {herbaMystica ? <p>Herba mystica sandwiches here</p> : <p>non-HM sandwiches here</p>}
+        </div>
     )
 }
