b94c70f4d3a8a5d392345dc9a1effd49f5baf1a7
JackDixon019 <126362891+JackDixon019@users.noreply.github.com>
2023-10-13 16:28:18 +1100

    Sandwiches (#21)
    * Feat: added sandwiches dictionary, added rudimentary HM toggle button
    
    * Feat: sandwich page funcitonal. No styling
    
    * Edit: little bit more readbale
    
    * added some css
    
    * feat: separated sandwich recipes to their own component
    
    * Fix: corrected issue with types in apiCall
    
    * added sandwiches to pokemonPage

--- src/pages/PokemonPage.jsx
+++ src/pages/PokemonPage.jsx
@@ -1,14 +1,25 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ActivePokemon } from "../contexts/activePokemon";
 import fetchData from "../components/apiCall";
 import { ApiContext } from "../contexts/apiContext";
 import capitalise from "../components/capitalise";
+import SandwichRecipe from "../components/SandwichRecipe";
+import outbreakPokemon from "../components/outbreaksList";
 
 export default function PokemonPage() {
     let { pokemonName } = useParams();
     let { activePokemon, setActivePokemon } = useContext(ActivePokemon);
     let { apiUrl } = useContext(ApiContext);
+
+    let [herbaMystica, setHerbaMystica] = useState(false);
+    function handleClickHM() {
+        setHerbaMystica(true);
+    }
+
+    function handleClickNonHM() {
+        setHerbaMystica(false);
+    }
 
     useEffect(() => {
         setActivePokemon("");
@@ -28,14 +39,14 @@ export default function PokemonPage() {
             <div id="pokemonPage">
                 <h1>{capitalise(pokemonName)}</h1>
                 <div>
+                    {/* Includes warning element if pokemon has self-damaging moves */}
                     {activePokemon.warning.length > 0 ? (
                         <div id="warnings">
                             <h3>Warning! This pokemon may have the following moves:</h3>
                             <ul>
-
-                            {activePokemon.warning.map((element) => {
-                                return <li key={element}>{capitalise(element)}</li>;
-                            })}
+                                {activePokemon.warning.map((element) => {
+                                    return <li key={element}>{capitalise(element)}</li>;
+                                })}
                             </ul>
                         </div>
                     ) : (
@@ -43,23 +54,48 @@ export default function PokemonPage() {
                     )}
                     <div id="pokemonPreviews">
                         <div className="preview" id="defaultPreview">
-                            <img alt="default sprite" id="default_sprite" src={activePokemon.sprites.front_default} />
+                            <img
+                                alt="default sprite"
+                                id="default_sprite"
+                                src={activePokemon.sprites.front_default}
+                            />
                             <h3>Default</h3>
                         </div>
                         <div className="preview" id="shinyPreview">
-                            <img alt="shiny sprite" id="shiny_sprite" src={activePokemon.sprites.front_shiny} />
+                            <img
+                                alt="shiny sprite"
+                                id="shiny_sprite"
+                                src={activePokemon.sprites.front_shiny}
+                            />
                             <h3>Shiny</h3>
                         </div>
                     </div>
-                    <div className="info" id="sandwich">
+                    <div className="info">
+                        <h3>Outbreaks? {outbreakPokemon.includes(capitalise(activePokemon.name)) ? '✔' : '❌'}</h3>
+                    </div>
+                    <div className="info" id="sandwichPage">
                         <h3>Recommended sandwiches:</h3>
-                        <h4>Non-Herba Mystica: </h4>
-                        <div className="sandwich">
-                            sandwich here
+                        {/* className is activeSandwich because react-router uses the active class for navLinks */}
+                        <div id="buttonsDiv">
+                            <button
+                                id="herbaMysticaButton"
+                                onClick={handleClickHM}
+                                className={herbaMystica ? "activeSandwich" : ""}
+                            >
+                                Herba Mystica
+                            </button>
+                            <button
+                                id="nonHerbaMysticaButton"
+                                onClick={handleClickNonHM}
+                                className={!herbaMystica ? "activeSandwich" : ""}
+                            >
+                                Non-Herba Mystica
+                            </button>
                         </div>
-                        <h4>With Herba Mystica: </h4>
-                        <div className="sandwich">
-                            sandwich here
+                        <div id="recipesDiv">
+                            {activePokemon.types.map((type) => {
+                                return <div>{SandwichRecipe(type, herbaMystica)}</div>;
+                            })}
                         </div>
                     </div>
                 </div>
--- src/pages/Sandwiches.jsx
+++ src/pages/Sandwiches.jsx
@@ -1,5 +1,44 @@
-export default function SandwichesPage(){
-    return(
-        <h1>SandwichesPage</h1>
-    )
+import { useState } from "react";
+import sandwichesDictionary from "../components/sandwichesDictionary";
+import SandwichRecipe from "../components/SandwichRecipe";
+
+export default function SandwichesPage() {
+    let [herbaMystica, setHerbaMystica] = useState(false);
+    function handleClickHM() {
+        setHerbaMystica(true);
+    }
+
+    function handleClickNonHM() {
+        setHerbaMystica(false);
+    }
+    let types = Object.keys(sandwichesDictionary);
+
+    return (
+        <div id="sandwichPage">
+            <h1>SandwichesPage</h1>
+            {/* className is activeSandwich because react-router uses the active class for navLinks */}
+            <div id="buttonsDiv">
+                <button
+                    id="herbaMysticaButton"
+                    onClick={handleClickHM}
+                    className={herbaMystica ? "activeSandwich" : ""}
+                >
+                    Herba Mystica
+                </button>
+                <button
+                    id="nonHerbaMysticaButton"
+                    onClick={handleClickNonHM}
+                    className={!herbaMystica ? "activeSandwich" : ""}
+                >
+                    Non-Herba Mystica
+                </button>
+            </div>
+            <div id="recipesDiv">
+                {/* This creates an array of types, with each type having an associated sandwich. */}
+                {types.map((type) => {
+                    return <div>{SandwichRecipe(type, herbaMystica)}</div>;
+                })}
+            </div>
+        </div>
+    );
 }
