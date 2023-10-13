2bafc6d941958fca372eb739ef30ddb22971c3be
JackDixon019 <13709@coderacademy.edu.au>
2023-10-14 00:51:36 +1100

    timer css

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,62 +1,64 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { ActiveTimer } from '../contexts/timerStatus';
-import { TimerToggle } from '../contexts/timerToggle';
+import React, { useState, useEffect, useContext } from "react";
+import { ActiveTimer } from "../contexts/timerStatus";
+import { TimerToggle } from "../contexts/timerToggle";
 
 function Stopwatch() {
-  const [isRunning, setIsRunning] = useState(true);
-  const { activeTimer, setActiveTimer } = useContext(ActiveTimer);
-  const {timerStatus, setTimerStatus} = useContext(TimerToggle)
+    const [isRunning, setIsRunning] = useState(true);
+    const { activeTimer, setActiveTimer } = useContext(ActiveTimer);
+    const { timerStatus, setTimerStatus } = useContext(TimerToggle);
 
-  useEffect(() => {
-    let interval;
+    useEffect(() => {
+        let interval;
 
-    if (isRunning) {
-      interval = setInterval(() => {
-        setActiveTimer((prevTime) => prevTime + 1);
-      }, 1000);
-    } else {
-      clearInterval(interval);
-    }
+        if (isRunning) {
+            interval = setInterval(() => {
+                setActiveTimer((prevTime) => prevTime + 1);
+            }, 1000);
+        } else {
+            clearInterval(interval);
+        }
 
-    return () => {
-      clearInterval(interval);
+        return () => {
+            clearInterval(interval);
+        };
+        // eslint-disable-next-line
+    }, [isRunning]);
+
+    const handleButtonClick = () => {
+        setIsRunning((prevIsRunning) => !prevIsRunning);
     };
-    // eslint-disable-next-line
-  }, [isRunning]);
 
-  const handleButtonClick = () => {
-    setIsRunning((prevIsRunning) => !prevIsRunning);
-  };
+    const resetStopwatch = () => {
+        setIsRunning(false);
+        setActiveTimer(0);
+        setTimerStatus(false);
+    };
 
-  const resetStopwatch = () => {
-    setIsRunning(false);
-    setActiveTimer(0);
-    setTimerStatus(false)
-  };
-
-  return (
-    <div className={'timer ' + (timerStatus ? 'visible' : 'hidden')} >
-      <span>{formatTime(activeTimer)}</span>
-      <div id='wrapper'>
-        <button onClick={handleButtonClick} className={'timerButton ' + (isRunning ? 'stopButton' : 'activeSandwich')}>
-          {isRunning ? "Stop" : "Start"}
-        </button>
-        <button onClick={resetStopwatch}>Finish Hunt</button>
-      </div>
-
-    </div>
-  );
+    return (
+        <div className={"timer " + (timerStatus ? "visible" : "hidden")}>
+            <span>{formatTime(activeTimer)}</span>
+            <div id="wrapper">
+                <button
+                    onClick={handleButtonClick}
+                    className={"timerButton " + (isRunning ? "stopButton" : "activeSandwich")}
+                >
+                    {isRunning ? "Stop" : "Start"}
+                </button>
+                <button onClick={resetStopwatch}>Finish Hunt</button>
+            </div>
+        </div>
+    );
 }
 
 function formatTime(seconds) {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
 
-  return `${padWithZero(minutes)}:${padWithZero(remainingSeconds)}`;
+    return `${padWithZero(minutes)}:${padWithZero(remainingSeconds)}`;
 }
 
 function padWithZero(value) {
-  return value < 10 ? `0${value}` : value;
+    return value < 10 ? `0${value}` : value;
 }
 
 export default Stopwatch;
