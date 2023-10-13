7416e8af269132f6772d2b58bd39a58c83a7243e
JackDixon019 <13709@coderacademy.edu.au>
2023-10-13 18:19:42 +1100

    Styling: Styled on 'em

--- src/pages/Guide.jsx
+++ src/pages/Guide.jsx
@@ -1,5 +1,57 @@
-export default function GuidePage(){
-    return(
-        <h1>GuidePage</h1>
-    )
+export default function GuidePage() {
+    return (
+        <div id="guidePage">
+            <h1>GuidePage</h1>
+            <h1>How to find a shiny Pokémon using Mass Outbreaks</h1>
+            <p>
+                Shiny hunting in Mass Outbreaks is advisable (if you have an outbreak of interest)
+                as shiny rates are higher. To do so, you’ll need to do the following.
+            </p>
+            <ol>
+                <li>
+                    Find the Mass Outbreak that you want to shiny hunt. (You can change the date on
+                    your Nintendo Switch to get a different set of outbreaks. They also change at
+                    midnight.)
+                </li>
+
+                <li>Save your game in case something goes wrong.</li>
+
+                <li>
+                    Defeat 60 of the mass outbreak Pokémon. There is no way to 100% track this in
+                    game, so you’ll need to manually count and keep tabs on it. There will be
+                    messages that note that the number of Pokémon in the outbreak is decreasing
+                    every 20-30 Pokémon defeated, so you can use this to help keep track. When it
+                    says the number of Pokémon is “definitely getting lower,” that means you’ve
+                    knocked out at least 50 Pokémon. Be careful not to overhunt the Pokémon, as this
+                    can cause the outbreak to end. This typically requires you to defeat over 100 of
+                    the Pokémon. (Restart your game if this happens.) Note that this is not a
+                    “chain” as used in previous Pokémon games. You can battle other Pokémon between
+                    the 60, run from battles, leave the outbreak and come back, etc. There is no
+                    “chain” to break.
+                </li>
+                <li>Save your game again once you’re sure you’ve defeated at least 60.</li>
+                <li>
+                    If you have Herba Mystica, make a sandwich for the outbreak Pokémon’s type using
+                    the recipes above. Rates are quite high even without the Sparkling Power
+                    sandwich, so don’t stress if you don’t have any Herba Mystica.
+                </li>
+                <li>
+                    Run in and out of the outbreak area to despawn and respawn the Pokémon,
+                    refreshing for shinies. If you’re hunting for a Pokémon where it’s hard to tell
+                    if it’s shiny, you can use the auto-battle feature, as your Pokémon will refuse
+                    to fight the wild Pokémon if it’s shiny.
+                </li>
+                <li>
+                    If the outbreak ends (from accidentally defeating too many) or if your 30-minute
+                    sandwich window expires, close your game and repeat steps 5-7 until you get your
+                    desired shiny.
+                </li>
+            </ol>
+            If the Pokémon is in a place where it’s convenient to set up a picnic, you can set up
+            and then quickly take down a picnic to despawn and respawn all the Pokémon in the area,
+            allowing you to quickly look for shinies. This method is referred to as the “picnic
+            method,” but it obviously doesn’t work in water or on some inconveniently steep cliff
+            sides.
+        </div>
+    );
 }
