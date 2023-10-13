38de3f712e1b95a6bcd48d980f0a5cc41e16a8c4
JackDixon019 <13709@coderacademy.edu.au>
2023-10-13 18:39:09 +1100

    Fix: fixed some linter issues

--- src/pages/OutbreakCounter.jsx
+++ src/pages/OutbreakCounter.jsx
@@ -20,9 +20,10 @@ export default function OutbreakCounterPage(){
             {count > 45 && count <= 55 && <span>That shiny is on the horizon!</span>}
             {count > 55 && count <= 59 && <span>{60 - count} more to go!</span>}
             {count >= 60 && <span>DANGER ZONE!</span>}
-            <button className="increment" onClick={ () => setCount(count+1)}><img src={activePokemon.sprites.front_default} /></button>
-            <button className="decrement" onClick={ () => setCount(count == 0 ? count = 0 : count - 1)}>-</button>
+            <button className="increment" onClick={ () => setCount(count+1)}><img alt="frontDefaultSprite" src={activePokemon.sprites.front_default} /></button>
+            <button className="decrement" onClick={ () => setCount(count === 0 ? count = 0 : count - 1)}>-</button>
             <button className="reset" onClick={ () => setCount(count=0)}>X</button>
+            <Stopwatch />
             </div>
         )
       } else {
@@ -30,7 +31,7 @@ export default function OutbreakCounterPage(){
             <div className="button-wrapper">
             <h1>OutbreakCounterPage</h1>
             <h2>{count}/60</h2>
-            {count == 0 && <span>Let the shiny hunt begin!</span>}
+            {count === 0 && <span>Let the shiny hunt begin!</span>}
             {count >= 1 && count <= 10 && <span>The start of something great</span>}
             {count > 10 && count <= 30 && <span>Those rascals don't know what hit them!</span>}
             {count > 30 && count <= 45 && <span>More than halfway there!</span>}
